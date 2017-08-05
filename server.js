47853ba9151b38e4f196d808b19fd7c99f518358
Douglas Lyford <Douggyfr3sh@gmail.com>
2017-08-05 17:11:02 -0400

    added http module in server.js and added more server start code

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var http = require('http');
 
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
@@ -10,14 +11,19 @@ var db = require('./db/config');
 // see routes line 144 for description
 var specialHashRegex = '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
 
+app.set('port', process.env.PORT || 3000);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
-
 app.use(require('./routes'));
 
+// Start server
+http.createServer(app).listen(app.get('port'), function (req, res) {
+  console.log('Express server listening on port ' + app.get('port'));
+//to do - error handling
+});
 
 
 
-app.set('port', process.env.PORT || 3000);
-console.log('server running');
+console.log('server started ', Date());
